7f9a23f6272c47133d681bb86149f7a90a67fd3c
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2024-04-20 14:58:57 +0100

    updates

--- README.md
+++ README.md
@@ -1,18 +1,3 @@
 # ecombuy
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
-
 [![wakatime](https://wakatime.com/badge/user/018ef58f-c7fd-41e7-b5f0-4f86376381e5/project/018ef5b6-0961-43eb-931b-a3bd21693c01.svg)](https://wakatime.com/badge/user/018ef58f-c7fd-41e7-b5f0-4f86376381e5/project/018ef5b6-0961-43eb-931b-a3bd21693c01)
--- lib/unlogged_screen.dart
+++ lib/unlogged_screen.dart
@@ -151,6 +151,70 @@ class UnloggedScreen extends StatelessWidget {
                         image: AssetImage(
                           "images/banner2.png",
                         ))),
+              ),
+              Products(
+                features: CategoryFeatures(
+                  categoryText1: Text(
+                    "Top Rated  ",
+                    style: TextStyle(fontSize: 20, color: textBlack),
+                  ),
+                  categoryText2: Text(
+                    "see all ",
+                    style: TextStyle(fontSize: 16, color: textBlue),
+                  ),
+                  categoryFunction1: () {},
+                  categoryFunction2: () {},
+                ),
+                card1: ProductCard(
+                  product_image: Image(image: AssetImage("images/headset.png")),
+                  product_text: Text("Wireless Headset"),
+                  button_text: Text("Add"),
+                  product_price: Text("USD 5000.00"),
+                ),
+                card2: ProductCard(
+                  product_image: Image(image: AssetImage("images/drill.png")),
+                  product_text: Text("Wireless Headset"),
+                  button_text: Text("Add"),
+                  product_price: Text("USD 5000.00"),
+                ),
+                bannerScroll: const BannerScroll(
+                    banner_image: Image(
+                        fit: BoxFit.contain,
+                        image: AssetImage(
+                          "images/Banner1.png",
+                        ))),
+              ),
+              Products(
+                features: CategoryFeatures(
+                  categoryText1: Text(
+                    " New Arrivals  ",
+                    style: TextStyle(fontSize: 20, color: textBlack),
+                  ),
+                  categoryText2: Text(
+                    "see all ",
+                    style: TextStyle(fontSize: 16, color: textBlue),
+                  ),
+                  categoryFunction1: () {},
+                  categoryFunction2: () {},
+                ),
+                card1: ProductCard(
+                  product_image: Image(image: AssetImage("images/headset.png")),
+                  product_text: Text("Wireless Headset"),
+                  button_text: Text("Add"),
+                  product_price: Text("USD 5000.00"),
+                ),
+                card2: ProductCard(
+                  product_image: Image(image: AssetImage("images/drill.png")),
+                  product_text: Text("Wireless Headset"),
+                  button_text: Text("Add"),
+                  product_price: Text("USD 5000.00"),
+                ),
+                bannerScroll: const BannerScroll(
+                    banner_image: Image(
+                        fit: BoxFit.contain,
+                        image: AssetImage(
+                          "images/banner2.png",
+                        ))),
               )
             ],
           ),
